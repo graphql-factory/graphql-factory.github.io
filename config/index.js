2358d8103402e3833b6bbaf6d51f010cb570bffd
Branden Horiuchi <bhoriuchi@gmail.com>
2017-11-02 21:57:09 -0700

    removing graphql-factory path

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     index: path.resolve(__dirname, '../index.html'),
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/graphql-factory/docs/',
+    assetsPublicPath: '/docs/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
