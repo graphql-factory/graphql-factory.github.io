b688c8d89956c74839540b20a149863a403d8830
Branden Horiuchi <bhoriuchi@gmail.com>
2017-11-02 22:26:29 -0700

    moving assets path

--- config/index.js
+++ config/index.js
@@ -9,9 +9,9 @@ module.exports = {
   build: {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../index.html'),
-    assetsRoot: path.resolve(__dirname, '../docs'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/docs/',
+    assetsRoot: path.resolve(__dirname, '../'),
+    assetsSubDirectory: 'assets',
+    assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
